c63130115029002d16dd2743cbba27dbd55c95f7
Paulo César dos Passos Pereira <paulo.passos1@gmail.com>
2024-09-12 07:33:18 -0300

    criação do repositório

---
+++ src/main/java/br/com/passos/controle_saida_caminhao/ControleSaidaCaminhaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.passos.controle_saida_caminhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleSaidaCaminhaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleSaidaCaminhaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/passos/controle_saida_caminhao/ControleSaidaCaminhaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passos.controle_saida_caminhao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleSaidaCaminhaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
